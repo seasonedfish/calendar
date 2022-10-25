e3dbfe88e229b6c2b01d0afef105afbdba0a8108
Fisher Sun <f.sun@wustl.edu>
2022-10-24 20:07:01 -0500

    Start updateCalendar

--- js/main.js
+++ js/main.js
@@ -18,10 +18,28 @@ function getDecrementedDate(date) {
     );
 }
 
-let viewDate = new Date();
-export { viewDate };
+function getDaysInMonth(date) {
+    // https://stackoverflow.com/a/1184359
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
 
+function updateCalendar(date) {
+    let daysInMonth = getDaysInMonth(date);
 
+    let days = [];
+    const template = document.querySelector("#calendar-day");
+    for (let d = 1; d <= daysInMonth; d++) {
+        const clone = template.content.cloneNode(true);
 
+        const dayNumber = clone.querySelector("#day-number");
+        dayNumber.textContent = d.toString();
 
+        days.push(clone);
+    }
 
+    const dayGrid = document.querySelector("#day-grid");
+    dayGrid.replaceChildren(...days);
+}
+
+let viewDate = new Date("2022-02-02");
+updateCalendar(viewDate);
