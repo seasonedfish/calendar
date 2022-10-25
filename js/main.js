14b067d56351ac30fe66b5c161d497142100439f
Fisher Sun <f.sun@wustl.edu>
2022-10-24 23:07:19 -0500

    Try changing CSS using JS

--- js/main.js
+++ js/main.js
@@ -23,6 +23,16 @@ function getDaysInMonth(date) {
     return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
 
+function getStyleSheet(unique_title) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/styleSheets
+    for (const sheet of document.styleSheets) {
+        if (sheet.title === unique_title) {
+            return sheet;
+        }
+    }
+}
+
+
 function updateCalendar(date) {
     let daysInMonth = getDaysInMonth(date);
 
@@ -39,6 +49,11 @@ function updateCalendar(date) {
 
     const dayGrid = document.querySelector("#day-grid");
     dayGrid.replaceChildren(...days);
+
+    let sheet = getStyleSheet("First Day");
+    sheet.deleteRule(0);
+    sheet.insertRule("#day-grid li:first-of-type { grid-column-start: 5; }", 0);
+
 }
 
 let viewDate = new Date("2022-02-02");
