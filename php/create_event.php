8037efc307d2ef20eb13f90a8d9d48d8ffb07d6c
Harry Gao <harrygao@wustl.edu>
2022-10-26 04:30:46 +0000

    create event php

--- php/create_event.php
+++ php/create_event.php
@@ -1,8 +1,24 @@
 <?php
-/**
- * Takes in an event's details, and adds a new event in the database.
- */
+    /**
+     * Creates an event
+     */
 
-require_once "util.php";
+    header("Content-Type: application/json");
+    require_once "util.php";
 
-$sql_helper = new SqlHelper();
+    session_start();
+    $username = $_SESSION['username'];
+
+    $json_str = file_get_contents('php://input');
+    $json_obj = json_decode($json_str, true);
+
+    $title = $json_obj['title'];
+    $datetime = $json_obj['datetime'];
+    $location = $json_obj['location'];
+
+    $sql_helper = new SqlHelper();
+    $query = "insert into events (title, datetime, username, location) values (?, ?, ?, ?)";
+    $result = $sql_helper->get_query_result($query, "ssss", $title, $datetime, $username, $location);
+
+    send_data_as_json($result);
+?>
