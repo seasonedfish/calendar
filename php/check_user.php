42ace63dd7b02b15c0fd00d50888bc2d476f5e5d
Harry Gao <harrygao@wustl.edu>
2022-10-26 06:37:52 +0000

    get event

--- php/check_user.php
+++ php/check_user.php
@@ -3,6 +3,8 @@
      * Check if which user is signed in (if any)
      */
     require_once "util.php";
+    header("Content-Type: application/json");
+
 
     session_start();
     if (isset($_SESSION['username'])) {
--- php/get_event.php
+++ php/get_event.php
@@ -4,5 +4,20 @@
  */
 
 require_once "util.php";
+header("Content-Type: application/json");
 
-$sql_helper = new SqlHelper();
+if (isset($_GET['event_id'])) {
+    session_start();
+    if (isset($_SESSION['username'])) {
+        $event_id = $_GET['event_id'];
+        $username = $_SESSION['username'];
+
+        $sql_helper = new SqlHelper();
+        $query = "select * from events where event_id=?";
+        $result = $sql_helper->get_query_result($query, "i", $event_id)[0];
+
+        if ($result['username'] == $username) {
+            send_data_as_json($result);
+        }
+    }
+}
