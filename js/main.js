2ccc2b7dd239afeeaf3b25ddb9646d50c4c96c83
Fisher Sun <f.sun@wustl.edu>
2022-10-25 19:16:20 -0500

    Try to add days to the calendar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 /**
  * The client-side code for the calendar main screen.
  */
+
+const BACKEND_PREFIX = "dummy"
+
+/**
+ * Updates the month and year in the header.
+ */
 function updateHeader() {
     const monthAndYear = document.getElementById("month-and-year");
 
@@ -10,34 +16,79 @@ function updateHeader() {
     monthAndYear.innerHTML = `${monthString} ${year}`;
 }
 
+/**
+ * Switches the calendar to the previous month, then updates everything.
+ */
 function previousMonth() {
     viewDate = new Date(viewDate.getFullYear(), viewDate.getMonth() - 1, 1);
     updateAll();
 }
 
+/**
+ * Switches the calendar to the next month, then updates everything.
+ */
 function nextMonth() {
     viewDate = new Date(viewDate.getFullYear(), viewDate.getMonth() + 1, 1);
     updateAll();
 }
 
+/**
+ * Helper method that returns the number of days in the month.
+ * @param date a Date object
+ * @returns {number}
+ */
 function getDaysInMonth(date) {
     // https://stackoverflow.com/a/1184359
     return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 }
 
-function updateCalendar() {
-    let daysInMonth = getDaysInMonth(viewDate);
+async function fetchEvents() {
+    const response = await fetch(`${BACKEND_PREFIX}/get_month_events.php`, {});
+    return await response.json();
+}
 
+function getArrayOfEmptyDays(daysInMonth) {
     let days = [];
     const template = document.querySelector("#calendar-day");
-    for (let d = 1; d <= daysInMonth; d++) {
+    for (let i = 0; i < daysInMonth; i++) {
         const clone = template.content.cloneNode(true);
 
         const dayNumber = clone.querySelectorAll(".day-number")[0];
-        dayNumber.textContent = d.toString();
+        dayNumber.textContent = (i + 1).toString();
 
         days.push(clone);
     }
+    return days;
+}
+
+/**
+ * Updates the calendar grid.
+ */
+function updateCalendar() {
+    let daysInMonth = getDaysInMonth(viewDate);
+    let days = getArrayOfEmptyDays(daysInMonth);
+
+    fetchEvents()
+        .then(
+            events => {
+                for (const event of events) {
+                    let eventDate = new Date(event["datetime"]);
+                    let dayNumber = eventDate.getDay();
+
+                    let day = days[dayNumber - 1];
+                    let eventsList = day.querySelectorAll(".events-list")[0];
+
+                    let li = document.createElement("li");
+                    let a = document.createElement("a");
+                    a.setAttribute("href", "javascript:void(0)");
+                    a.textContent = `${eventDate.getHours()}:${eventDate.getMinutes()} ${event["title"]}`;
+                    li.replaceChildren(a);
+
+                    eventsList.appendChild(li);
+                }
+            }
+        )
+        .catch(error => console.error('Error:', error))
 
     const dayGrid = document.querySelector("#day-grid");
     dayGrid.replaceChildren(...days);
