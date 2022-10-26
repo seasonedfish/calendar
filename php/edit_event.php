6fb3b829ba9e2dc4ae252468671a7d14163c1fb7
Harry Gao <harrygao@wustl.edu>
2022-10-26 06:48:12 +0000

    asd

--- php/edit_event.php
+++ php/edit_event.php
@@ -1,8 +1,22 @@
 <?php
-/**
- * Takes in an event ID and its information, and updates that event in the database.
- */
+    /**
+     * Edits an event
+     */
 
-require_once "util.php";
+    header("Content-Type: application/json");
+    require_once "util.php";
 
-$sql_helper = new SqlHelper();
+    session_start();
+    $username = $_SESSION['username'];
+
+    $json_str = file_get_contents('php://input');
+    $json_obj = json_decode($json_str, true);
+
+    $title = $json_obj['title'];
+    $datetime = $json_obj['datetime'];
+    $location = $json_obj['location'];
+
+    $sql_helper = new SqlHelper();
+    $query = "insert into events (title, datetime, username, location) values (?, ?, ?, ?)";
+    $sql_helper->execute_query($query, "ssss", $title, $datetime, $username, $location);
+?>
